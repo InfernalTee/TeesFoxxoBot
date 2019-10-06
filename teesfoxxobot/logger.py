d9d3e8543b50f63dee912d48df276f869c8ab789
Sean Collins <seanc@seancotech.com>
2019-10-06 16:18:12 -0700

    Initial commit

---
+++ teesfoxxobot/logger.py
@@ -0,0 +1,8 @@
+from logging import getLogger, StreamHandler, FileHandler, DEBUG
+from sys import stdout
+
+
+log = getLogger(__name__)
+log.setLevel(DEBUG)
+log.addHandler(StreamHandler(stdout))
+log.addHandler(FileHandler('foxxo_bot.log'))
