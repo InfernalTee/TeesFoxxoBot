765b08f565fcabf8b1687f99462b3ce8bbb7c5b2
InfernalTee <52016337+InfernalTee@users.noreply.github.com>
2019-10-12 17:24:09 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace FoxxoBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("");
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+        }
+    }
+}
